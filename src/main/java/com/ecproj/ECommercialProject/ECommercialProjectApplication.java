5cb35127e934fecea5fe6595fe32b658cb6dfa14
Elijah <jsuielie@gmail.com>
2024-05-11 17:18:30 +0800

    feat(entry-point): set up application entry-point and pom file

---
+++ src/main/java/com/ecproj/ECommercialProject/ECommercialProjectApplication.java
@@ -0,0 +1,12 @@
+package com.ecproj.ECommercialProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommercialProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommercialProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ecproj/ecproj/EcprojApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ecproj.ecproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.ecproj.ECommercialProject.ECommercialProjectApplication;
+
+@SpringBootTest(classes = ECommercialProjectApplication.class)
+class EcprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
